c6fce72954e018c3030a31caf5f9a44bd6d44ca4
Glauberty Ribeiro <glaubertyribeiro007@gmail.com>
2026-05-10 11:46:57 -0300

    Criando a tabela de entidade do usuário

---
+++ src/main/java/com/myOficce/entity/Usuario.java
@@ -0,0 +1,86 @@
+package com.myOficce.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_usuario;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(length = 20, nullable = false)
+    private String telefone;
+
+    @Column(nullable = false, length = 100)
+    private String senha;
+
+    public Long getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }   
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Usuario() {
+    }
+    
+    public Usuario(String nome, String email, String telefone, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario[id_usuario=" + id_usuario +", nome= " + nome +", email= " + email +", telefone= " + telefone +", senha= " + senha +"]";
+    }
+
+
+}
