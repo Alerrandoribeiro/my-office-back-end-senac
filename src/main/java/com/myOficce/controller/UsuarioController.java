028354dea213a00c04c5b35b0427e897c8f0cfbb
Glauberty Ribeiro <glaubertyribeiro007@gmail.com>
2026-05-10 15:31:34 -0300

    Evitando problema de cors no controller

--- src/main/java/com/myOficce/controller/UsuarioController.java
+++ src/main/java/com/myOficce/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.myOficce.dto.UsuarioDTO;
 import com.myOficce.service.UsuarioService;
 
+@CrossOrigin(origins = "*") // evita problemas de CORS, permitindo que o frontend acesse a API mesmo estando em domínios diferentes
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
