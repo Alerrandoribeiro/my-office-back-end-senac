a2d0f01e78cfd7dbbe10e9acbfed2a526cce306d
Glauberty Ribeiro <glaubertyribeiro007@gmail.com>
2026-05-10 10:55:50 -0300

    Criando o backEnd com spring já com as dependências

---
+++ src/main/java/com/myOficce/MyOficceApplication.java
@@ -0,0 +1,13 @@
+package com.myOficce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOficceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOficceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myOficce/MyOficceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myOficce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOficceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
