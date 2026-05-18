33859107958c1743a48532bd8aff1dfce73c59d1
GLAUBERTY ALERRANDO CHAGAS RIBEIRO <glauberty.ribeiro@alunos.sc.senac.br>
2026-05-18 20:56:48 -0300

    finalizando o crud da reserva, porém precisa de ajuste no id_sala no entity

--- src/main/java/com/myOficce/repository/ReservaRepository.java
+++ src/main/java/com/myOficce/repository/ReservaRepository.java
@@ -3,6 +3,8 @@ package com.myOficce.repository;
 import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.myOficce.entity.Reserva;
@@ -10,5 +12,14 @@ import com.myOficce.entity.Sala;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-    boolean existsBySalaAndData(Sala sala, LocalDate data);
+  @Query("""
+        SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END
+        FROM Reserva r
+        WHERE r.sala = :sala
+        AND r.data = :data
+    """)
+    boolean existsBySalaAndData(
+            @Param("sala") Sala sala,
+            @Param("data") LocalDate data
+    );
 }
